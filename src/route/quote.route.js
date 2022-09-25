99dd7d1637d56287a3971fdfae97f5e82105aa57
DevCharles <charlesipn@gmail.com>
2022-09-25 01:17:34 -0500

    generate pdf from a webpage

--- src/route/quote.route.js
+++ src/route/quote.route.js
@@ -1,84 +1,111 @@
-const {getByQuoterId, createQuote, updateQuote, deleteQuote} = require("../usecases/quote.usecase")
-const express = require("express")
-const authMiddleware = require("../middlewares/auth.middlewares")
+const {
+  getByQuoterId,
+  createQuote,
+  updateQuote,
+  deleteQuote,
+  toPDF,
+} = require("../usecases/quote.usecase");
+const fs = require("fs");
+const express = require("express");
+const authMiddleware = require("../middlewares/auth.middlewares");
 
-const router = express.Router()
+const router = express.Router();
 
 //Middleware de auth
-router.use(authMiddleware)
+router.use(authMiddleware);
 
 router.get("/", async (request, response) => {
-    try{
-        const quotes= await getByQuoterId(request.quoter.id)
-        response.json({
-            success:true,
-            data:{
-                quotes
-            }
-        })
-    }catch (err){
-        response.status(err.status || 500)
-        response.json({
-            succes:false,
-            message: err.message
-        })
-    }
-})
+  try {
+    const quotes = await getByQuoterId(request.quoter.id);
+    response.json({
+      success: true,
+      data: {
+        quotes,
+      },
+    });
+  } catch (err) {
+    response.status(err.status || 500);
+    response.json({
+      succes: false,
+      message: err.message,
+    });
+  }
+});
 
-router.post("/", async(request, response) =>{
-    try{
-        const quote = await createQuote(request.quoter.id, request.body)
-        response.json({
-            success:true,
-            data:{
-                quote
-            }
-        })
-    }catch(err){
-        response.status(err.status || 500)
-        response.json({
-            succes:false,
-            message: err.message
-        })
-    }
+router.get("/:id/pdf", async (request, response) => {
+  try {
+    const { id } = request.params;
 
-})
+    const pdfPath = await toPDF(id);
 
-router.patch("/:id", async (request, response)=>{
-    const{id} = request.params
-    try{
-        const quote = await updateQuote(id, request.body)
-        response.json({
-            success:true,
-            data:{
-                quote
-            }
-        })
-    }catch(err){
-        response.status(err.status || 400)
-        response.json({
-            success : false,
-            message: err.message
-        })
-    }
-})
+    var file = fs.createReadStream(pdfPath);
+    var stat = fs.statSync(pdfPath);
+    response.setHeader("Content-Length", stat.size);
+    response.setHeader("Content-Type", "application/pdf");
+    response.setHeader("Content-Disposition", "attachment; filename=quote.pdf");
+    file.pipe(response);
+  } catch (err) {
+    response.status(err.status || 500);
+    response.json({
+      succes: false,
+      message: err.message,
+    });
+  }
+});
 
-router.delete("/:id", async(request, response)=>{
-    const {id} = request.params
-    try{
-        const quote = await deleteQuote(id)
-        response.status(200)
-        response.json({
-            success:true,
-            message: "Quote Deleted"
-        })
-    }catch(err){
-        response.status(error.status || 500)
-        response.json({
-            success : false,
-            message: error.message
-        })
-    }
-})
+router.post("/", async (request, response) => {
+  try {
+    const quote = await createQuote(request.quoter.id, request.body);
+    response.json({
+      success: true,
+      data: {
+        quote,
+      },
+    });
+  } catch (err) {
+    response.status(err.status || 500);
+    response.json({
+      succes: false,
+      message: err.message,
+    });
+  }
+});
 
-module.exports = router
+router.patch("/:id", async (request, response) => {
+  const { id } = request.params;
+  try {
+    const quote = await updateQuote(id, request.body);
+    response.json({
+      success: true,
+      data: {
+        quote,
+      },
+    });
+  } catch (err) {
+    response.status(err.status || 400);
+    response.json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+router.delete("/:id", async (request, response) => {
+  const { id } = request.params;
+  try {
+    const quote = await deleteQuote(id);
+    response.status(200);
+    response.json({
+      success: true,
+      message: "Quote Deleted",
+    });
+  } catch (err) {
+    response.status(error.status || 500);
+    response.json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+module.exports = router;
--- src/usecases/quote.usecase.js
+++ src/usecases/quote.usecase.js
@@ -1,31 +1,53 @@
-const Quote = require("../models/quote.model")
-const {createQuoteRecipe}= require("./quote-recipe.usecase")
+const { chromium } = require("playwright");
+const fs = require("fs");
+const Quote = require("../models/quote.model");
+const { createQuoteRecipe } = require("./quote-recipe.usecase");
 
 const createQuote = async (quoterId, data) => {
-    data.quoterId=quoterId
-    const {recipes}= data
-    const quoteCreated= await Quote.create(data)
+  data.quoterId = quoterId;
+  const { recipes } = data;
+  const quoteCreated = await Quote.create(data);
 
-    for(const recipe of recipes){
-        await createQuoteRecipe({
-            quoteId: quoteCreated._id,
-            recipeId: recipe.id,
-            quantity: recipe.quantity
-        })
-    }
-    return quoteCreated
-}
+  for (const recipe of recipes) {
+    await createQuoteRecipe({
+      quoteId: quoteCreated._id,
+      recipeId: recipe.id,
+      quantity: recipe.quantity,
+    });
+  }
+  return quoteCreated;
+};
 
 const getByQuoterId = (quoterId) => {
-    return Quote.find({quoterId})
-}
+  return Quote.find({ quoterId });
+};
 
-const updateQuote = (id, data)=>{
-    return Quote.findByIdAndUpdate(id, data, {returnDocument:"after"})
-}
+const updateQuote = (id, data) => {
+  return Quote.findByIdAndUpdate(id, data, { returnDocument: "after" });
+};
 
 const deleteQuote = (id) => {
-    return Quote.findByIdAndDelete(id)
-}
+  return Quote.findByIdAndDelete(id);
+};
 
-module.exports = {getByQuoterId, createQuote, updateQuote, deleteQuote}
+const toPDF = async (quoteId = 123) => {
+  //   const quote = await Quote.findById(quoteId);
+  //   if (!quote) throw new Error("Cotización no encontrada");
+
+  const filePath = `pdfs/${quoteId}.pdf`;
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+  //   await page.goto(`https://opsurveyapp.com/quote/${quoteId}`);
+  await page.goto(`https://example.com`);
+  await page.pdf({ path: filePath });
+  await browser.close();
+  return filePath;
+};
+
+module.exports = {
+  getByQuoterId,
+  createQuote,
+  updateQuote,
+  deleteQuote,
+  toPDF,
+};
