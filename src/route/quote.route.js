823b96bc164becb580ab887655cfadd782751213
ErnestoGarSa <ernest0_10@hotmail.com>
2022-09-28 23:50:46 -0500

    Add paid out quote

--- src/route/quote.route.js
+++ src/route/quote.route.js
@@ -1,84 +1,107 @@
-const {getByQuoterId, createQuote, updateQuote, deleteQuote} = require("../usecases/quote.usecase")
-const express = require("express")
-const authMiddleware = require("../middlewares/auth.middlewares")
+const {
+  getByQuoterId,
+  createQuote,
+  updateQuote,
+  deleteQuote,
+  paidOutQuote,
+} = require("../usecases/quote.usecase");
+const express = require("express");
+const authMiddleware = require("../middlewares/auth.middlewares");
 
-const router = express.Router()
+const router = express.Router();
 
 //Middleware de auth
-router.use(authMiddleware)
+router.use(authMiddleware);
 
 router.get("/", async (request, response) => {
-    try{
-        const quotes= await getByQuoterId(request.quoter.id)
-        response.json({
-            success:true,
-            data:{
-                quotes
-            }
-        })
-    }catch (err){
-        response.status(err.status || 500)
-        response.json({
-            succes:false,
-            message: err.message
-        })
-    }
-})
+  try {
+    const quotes = await getByQuoterId(request.quoter.id);
+    response.json({
+      success: true,
+      data: {
+        quotes,
+      },
+    });
+  } catch (err) {
+    response.status(err.status || 500);
+    response.json({
+      succes: false,
+      message: err.message,
+    });
+  }
+});
 
-router.post("/", async(request, response) =>{
-    try{
-        const quote = await createQuote(request.quoter.id, request.body)
-        response.json({
-            success:true,
-            data:{
-                quote
-            }
-        })
-    }catch(err){
-        response.status(err.status || 500)
-        response.json({
-            succes:false,
-            message: err.message
-        })
-    }
+router.post("/", async (request, response) => {
+  try {
+    const quote = await createQuote(request.quoter.id, request.body);
+    response.json({
+      success: true,
+      data: {
+        quote,
+      },
+    });
+  } catch (err) {
+    response.status(err.status || 500);
+    response.json({
+      succes: false,
+      message: err.message,
+    });
+  }
+});
 
-})
+router.patch("/:id", async (request, response) => {
+  const { id } = request.params;
+  try {
+    const quote = await updateQuote(id, request.body);
+    response.json({
+      success: true,
+      data: {
+        quote,
+      },
+    });
+  } catch (err) {
+    response.status(err.status || 400);
+    response.json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
 
-router.patch("/:id", async (request, response)=>{
-    const{id} = request.params
-    try{
-        const quote = await updateQuote(id, request.body)
-        response.json({
-            success:true,
-            data:{
-                quote
-            }
-        })
-    }catch(err){
-        response.status(err.status || 400)
-        response.json({
-            success : false,
-            message: err.message
-        })
-    }
-})
+router.delete("/:id", async (request, response) => {
+  const { id } = request.params;
+  try {
+    const quote = await deleteQuote(id);
+    response.status(200);
+    response.json({
+      success: true,
+      message: "Quote Deleted",
+    });
+  } catch (err) {
+    response.status(error.status || 500);
+    response.json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
 
-router.delete("/:id", async(request, response)=>{
-    const {id} = request.params
-    try{
-        const quote = await deleteQuote(id)
-        response.status(200)
-        response.json({
-            success:true,
-            message: "Quote Deleted"
-        })
-    }catch(err){
-        response.status(error.status || 500)
-        response.json({
-            success : false,
-            message: error.message
-        })
-    }
-})
+router.put("/:id", async (request, response) => {
+  const { id } = request.params;
+  try {
+    const quote = await paidOutQuote(id);
+    response.status(202);
+    response.json({
+      success: true,
+      message: "Cotización pagada",
+    });
+  } catch (err) {
+    response.status(err.status || 500);
+    response.json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
 
-module.exports = router
+module.exports = router;
--- src/usecases/quote.usecase.js
+++ src/usecases/quote.usecase.js
@@ -1,46 +1,60 @@
 const QuoteRecipes = require("../models/quote-recipes.model");
-const Quote = require("../models/quote.model")
-const {createQuoteRecipe}= require("./quote-recipe.usecase")
+const Quote = require("../models/quote.model");
+const { createQuoteRecipe } = require("./quote-recipe.usecase");
 
 const getByQuoterId = async (quoterId) => {
-    const quoterQuotes= await Quote.find({quoterId}).lean()
+  const quoterQuotes = await Quote.find({ quoterId }).lean();
 
-    const quotePerQuotes= await Promise.all(
-        quoterQuotes.map((quote) => {
-            return QuoteRecipes.find({quoteId: quote._id}).populate(
-                "recipeId"
-            )
-        })
-    )
-    return quoterQuotes.map((quote, index) => {
-        return {
-            ...quote, 
-            quotes:quotePerQuotes[index]
-        }
+  const quotePerQuotes = await Promise.all(
+    quoterQuotes.map((quote) => {
+      return QuoteRecipes.find({ quoteId: quote._id }).populate("recipeId");
     })
-}
+  );
+  return quoterQuotes.map((quote, index) => {
+    return {
+      ...quote,
+      quotes: quotePerQuotes[index],
+    };
+  });
+};
 
 const createQuote = async (quoterId, data) => {
-    data.quoterId=quoterId
-    const {recipes}= data
-    const quoteCreated= await Quote.create(data)
+  data.quoterId = quoterId;
+  const { recipes } = data;
+  const quoteCreated = await Quote.create(data);
 
-    for(const recipe of recipes){
-        await createQuoteRecipe({
-            quoteId: quoteCreated._id,
-            recipeId: recipe.id,
-            quantity: recipe.quantity
-        })
-    }
-    return quoteCreated
-}
+  for (const recipe of recipes) {
+    await createQuoteRecipe({
+      quoteId: quoteCreated._id,
+      recipeId: recipe.id,
+      quantity: recipe.quantity,
+    });
+  }
+  return quoteCreated;
+};
 
-const updateQuote = (id, data)=>{
-    return Quote.findByIdAndUpdate(id, data, {returnDocument:"after"})
-}
+const updateQuote = (id, data) => {
+  return Quote.findByIdAndUpdate(id, data, { returnDocument: "after" });
+};
 
 const deleteQuote = (id) => {
-    return Quote.findByIdAndDelete(id)
-}
+  return Quote.findByIdAndDelete(id);
+};
 
-module.exports = {getByQuoterId, createQuote, updateQuote, deleteQuote}
+const paidOutQuote = async (quoteId) => {
+  const quote = await Quote.findById(quoteId);
+  if (!quote) throw new Error("Cotización no encontrada");
+
+  const updatedQuote = await Quote.findByIdAndUpdate(quoteId, {
+    paidOut: true,
+  });
+  return updatedQuote;
+};
+
+module.exports = {
+  getByQuoterId,
+  createQuote,
+  updateQuote,
+  deleteQuote,
+  paidOutQuote,
+};
